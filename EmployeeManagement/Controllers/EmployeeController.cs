821e0f8a12e634f059c7ca5ab6739ca585ba5531
david-vargas98 <edgar981125@live.com.mx>
2025-08-13 10:53:42 -0600

    feat: EmployeeController.
    The controller was created; its constructor uses DI to initialize the instance which will be calling the methods.
    
    Also, the CreateEmployee method was implemented.

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    [Route("api/[controller]")] // i.e. api/employee
+    [ApiController] // enables features such as automatic model validation, binding, etc.
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository; // we use an interface to allow for easier testing and mocking
+
+        // We use the Interface to access the repository methods to decouple the controller from the repository implementation
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository; // we inject the repository through the constructor
+        }
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            await _employeeRepository.AddEmployeeAsync(employee);
+
+            return Created(); // Returns a 201 Created status code
+        }
+    }
+}
