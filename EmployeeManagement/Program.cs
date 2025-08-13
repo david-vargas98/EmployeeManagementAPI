75e96a059fd9280c7b5b82d2140cba5ea24a94f5
david-vargas98 <edgar981125@live.com.mx>
2025-08-12 23:29:11 -0600

    feat: Registering and configuring context.
    AppDBContext is registered and configured by using an in-memory db called EmployeeDB.

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,3 +1,6 @@
+using EmployeeManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace EmployeeManagement
 {
     public class Program
@@ -5,6 +8,12 @@ namespace EmployeeManagement
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // This registers and configures the AppDBContext as a service to the application
+            builder.Services.AddDbContext<AppDBContext>( // adds a context AppDBContext which receives the constructor as DbContextOptions<AppDBContext>
+                options => options.UseInMemoryDatabase("EmployeeDB") // arrow function to say that DB is in memory and named "EmployeeDB"
+            );
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
