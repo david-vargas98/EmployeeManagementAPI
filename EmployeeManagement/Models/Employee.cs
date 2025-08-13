893218002f24e51dd9be33ef9819006101e8ab2c
david-vargas98 <edgar981125@live.com.mx>
2025-08-12 21:57:11 -0600

    feat: Models folder | Employee model.
    Creating the Models folder and also the Employee model along with its properties.

---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public String Position { get; set; }
+    }
+}
