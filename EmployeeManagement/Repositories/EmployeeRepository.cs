8f343760cf90bcefcedd288985e5e47cbcf58fa3
david-vargas98 <edgar981125@live.com.mx>
2025-08-13 00:50:24 -0600

    feat: Repositories folder | EmployeeRepositor.
    Creation of Repositories folder and the EmployeeRepository.
    
    Adding constructor and _context variable to initialize the AppDBContext and use it within the class.
    
    AddEmployeeAsync method implemented.

---
+++ EmployeeManagement/Repositories/EmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeManagement.Data;
+using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDBContext _context; // Private field to make AppDBContext accessible within the class
+        public EmployeeRepository(AppDBContext context) // Constructor that receives the AppDBContext instance from dependency injection on Program.cs
+        {
+            _context = context; // Readonly only allows us to initialize the field here in the constructor (just once)
+        }
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            // We use async to allow the method to run asynchronously and await keyword to wait for the operation to complete without blocking the thread
+            await _context.Employees.AddAsync(employee); // Adds the employee to the Employees DbSet in memory
+            await _context.SaveChangesAsync(); // Saves the changes to the database asynchronously
+        }
+    }
+}
