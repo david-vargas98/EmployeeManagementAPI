27df5031f397a10b6e79a5f3242aa4c36ea609ac
david-vargas98 <edgar981125@live.com.mx>
2025-08-13 15:32:24 -0600

    refactor | feat: Rename to GetEmployeeById | Returning the Employee Created.
    The CreatedAtAction method was used to return the Employee by calling the GetEmployeeById and sending its id to it.
    
    The name was changed from GetEmployeeByIdAsync to GetEmployeeById as ASP.NET Core MVC trims the suffix Async from action names by default.

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -29,7 +29,7 @@ namespace EmployeeManagement.Controllers
 
         // We need to specify for our route that we expect an ID in the URL
         [HttpGet("{id}")] // i.e. api/employee/1
-        public async Task<ActionResult<Employee>> GetEmployeeByIdAsync(int id)
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
         {
             var employee = await _employeeRepository.GetByIdAsync(id);
 
@@ -42,9 +42,11 @@ namespace EmployeeManagement.Controllers
         [HttpPost]
         public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
         {
-            await _employeeRepository.AddEmployeeAsync(employee);
+            await _employeeRepository.AddEmployeeAsync(employee); // Adds the new employee asynchronously
 
-            return Created(); // Returns a 201 Created status code
+            // CreatedAtAction: returns a response that says "hey I created the employee, if you want further details call GetEmployeeByIdAsync method
+            // and use the following id: "new { id = employee.Id }" to get that information, and finally, in http body we return the "employee" object
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
         }
     }
 }
