be92d832a5389cfae0b98c3d409ceaf632997528
david-vargas98 <edgar981125@live.com.mx>
2025-08-12 23:26:42 -0600

    feat: Data folder | AppDBContext.
    Data folder was added, and AppDBContext was created and implemented in it.

---
+++ EmployeeManagement/Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Data
+{
+    public class AppDBContext : DbContext // DBContext has the methods to add, update, deleter and query data
+    {
+        //The constructor allows us to register our AppDBContext inside of Program.cs as a service (dependency injection)
+        public AppDBContext(DbContextOptions<AppDBContext> options) // this receives options from the Program.cs file (configuration on how to connect to the database)
+            : base(options) // this passes the options to the base DbContext class
+        {
+        }
+    }
+}
